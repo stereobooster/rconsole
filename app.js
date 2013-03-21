75776852430f52ae209125bc7aa5de220d36d54f
stereobooster <stereobooster@gmail.com>
2013-03-21 17:19:23 +0200

    Disable debug info of socket.io

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = connect()
   .use(connect.static(__dirname + '/node_modules/socket.io-client/dist', { maxAge: oneDay }))
   .listen(3000);
 
-var io = require('socket.io').listen(8000);
+var io = require('socket.io').listen(8000, {'log level': 0});
 
 io.sockets.on('connection', function (socket) {
   console.log('client connected');
